4fdc27b5d31d78fd13adf5f666a9ee05b6e508b9
Salinger2333 <1565664814@qq.com>
2025-10-15 21:44:18 +0800

    使用label判断质数

--- 03_function/script.js
+++ 03_function/script.js
@@ -4,3 +4,16 @@ const output = document.querySelector('#output')
 input.addEventListener('keydown', (event) => {
     output.textContent = `You pressed: ${event.key}`
 })
+
+
+
+let showPrimes = function (limit) {
+
+    nextPrime: for (let i = 2; i < limit; i++) {
+        for (let j = 2; j < i; j++) {
+            if (i % j === 0) continue nextPrime
+        }
+        alert(i)
+    }
+}
+
