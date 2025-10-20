7868454ed36e02d8b823f22eb0ba4683f8996566
Salinger2333 <1565664814@qq.com>
2025-10-20 16:51:09 +0800

    finish game program

--- 05_rock-paper-scissors/script.js
+++ 05_rock-paper-scissors/script.js
@@ -1,11 +1,11 @@
 let choices;
-let hunmanScore, computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 function getComputerChoice() {
-    choices = ['rock', 'paper', 'scissors']
+    choices = ['rock', 'paper', 'scissor']
     let randomNumber = Math.floor(Math.random() * choices.length)
     return choices[randomNumber]
 }
-
 
 function getHumanChoice() {
     let askHumanChoice = window.prompt("Choose ur decision");
@@ -13,8 +13,30 @@ function getHumanChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
-
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+    if (humanChoice == computerChoice) {
+        return console.log("same choice!No one wins");
+    }
+    else if (humanChoice == "paper" && computerChoice == "rock"
+        || humanChoice == "rock" && computerChoice == "scissor"
+        || humanChoice == "scissor" && computerChoice == "paper") {
+        humanScore++
+        return console.log("You win!!");
+    }
+    else {
+        computerScore++
+        return console.log("computer wins!!");
+    }
 }
 
+function playGame() {
 
-getComputerChoice()
+    for (let i = 1; i <= 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
+
+}
+playGame()
