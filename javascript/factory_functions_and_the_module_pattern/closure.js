0497604ba28126e8bda1a7fa497ad108d139c69c
Salinger2333 <1565664814@qq.com>
2025-11-14 18:01:23 +0800

    添加闭包示例代码

---
+++ javascript/factory_functions_and_the_module_pattern/closure.js
@@ -0,0 +1,17 @@
+if (Math.random() > 0.5) {
+    var x = 1;
+} else {
+    var x = 2;
+}
+console.log(x);// 使用var没有错误,因为{}没有为var创建作用域
+
+function makeFunc() {
+  const name = "Mozilla";
+  function displayName() {
+    console.log(name);
+  }
+  return displayName;
+}
+
+const myFunc = makeFunc();
+myFunc();
