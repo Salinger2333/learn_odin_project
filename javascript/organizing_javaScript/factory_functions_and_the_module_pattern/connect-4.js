ea733d4d878c4159b6b91d9c298a5772a9d1de04
Salinger2333 <1565664814@qq.com>
2025-11-19 21:15:39 +0800

    change console way

--- javascript/organizing_javaScript/factory_functions_and_the_module_pattern/connect-4.js
+++ javascript/organizing_javaScript/factory_functions_and_the_module_pattern/connect-4.js
@@ -47,7 +47,7 @@ function Gameboard() {
   // 但在我们构建 UI 后就不再需要它了
   const printBoard = () => {
     const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
-    console.log(boardWithCellValues);
+    console.table(boardWithCellValues);
   };
 
   // 在这里，我们为应用程序的其余部分
