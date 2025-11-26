a1835a460fc0faeb2aa9495568f13d921f7954f6
Salinger2333 <1565664814@qq.com>
2025-11-26 14:20:09 +0800

    revisit wepack

--- javascript/organizing_javaScript/webpack-practice/src/index.js
+++ javascript/organizing_javaScript/webpack-practice/src/index.js
@@ -1,3 +1,11 @@
 import { greeting } from "./greeting.js";
+import './style.css';
+import odinImage from "../asset/resource/odin.png";
 
+const image = document.createElement("img");
+image.src = odinImage;
+const div = document.createElement('div')
+div.textContent = 'use webpack dev serve'
+document.body.appendChild(image);
+document.body.appendChild(div)
 console.log(greeting);
