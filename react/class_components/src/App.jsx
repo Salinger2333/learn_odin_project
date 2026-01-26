b88c42be7f503243a1423b087f4e825faa1eb968
Salinger2333 <1565664814@qq.com>
2026-01-26 13:07:53 +0800

    learn propTypes

--- react/class_components/src/App.jsx
+++ react/class_components/src/App.jsx
@@ -1,17 +1,24 @@
 
 import './App.css'
-import React from 'react';
+import React, { useState } from 'react';
 import FunctionalInput from './components/FunctionalInput';
 import ClassInput from './components/ClassInput';
 
 
 export default function App() {
+  const [heading, setHeading] = useState("Magnificent Monkeys")
+  const clickHandler = () => {
+    setHeading('Mesiiiii')
+  }
   return (
     <>
       {/* <FunctionalInput name="Functional component!" />
       <div className="divider" />
       <ClassInput name="Class based component!" /> */}
-      <h1>hi</h1>
+      <button type='button' onClick={clickHandler}>
+        Click Me
+      </button>
+      <h1>{heading}</h1>
     </>
   );
 }
