8b5e4278e3e95d6abd85c7dc46333bbc61687338
Salinger2333 <1565664814@qq.com>
2026-03-13 19:24:37 +0800

    learn ejs

--- node/express/app.js
+++ node/express/app.js
@@ -2,7 +2,17 @@ import express from "express";
 import authorRouter from "./routes/authorRoutes.js";
 import bookRouter from "./routes/bookRoutes.js";
 import indexRouter from "./routes/indexRoutes.js";
+import path from "node:path";
+
 const app = express();
+
+const viewsPath = path.join(import.meta.dirname, "views");
+const assetsPath = path.join(import.meta.dirname,"public");
+
+app.set("views", viewsPath);
+app.set("view engine", "ejs");
+
+app.use(express.static(assetsPath));
 
 app.use("/author", authorRouter);
 app.use("/book", bookRouter);
--- node/express/routes/indexRoutes.js
+++ node/express/routes/indexRoutes.js
@@ -1,11 +1,20 @@
 import { Router } from "express";
 const indexRouter = Router();
 
+const links = [
+  { href: "/", text: "Home" },
+  { href: "about", text: "About" },
+];
+
+const users = ["Rose", "Cake", "Biff"];
+
 indexRouter
-  .get("/", (req, res) => res.send("this is index and idont know what is it"))
+  .get("/", (req, res) => {
+    res.render("index", { links: links, users });
+  })
   .get("/:index", (req, res) => {
     const { index } = req.params;
     res.send(`index id is ${index}`);
   });
 
-export default indexRouter
+export default indexRouter;
