0c9d86598a29b0193017edfc9eec6305f3420c19
Salinger2333 <1565664814@qq.com>
2026-07-10 15:40:14 +0800

    add refresh token, retry mechanism, server error handler

--- typescript/axios/req.ts
+++ typescript/axios/req.ts
@@ -13,9 +13,15 @@ type ResponseData<T = any> = {
   code: number;
   message: string;
 };
+
+const refreshToken = (): string => {
+  return "newToken";
+};
+const showError = (string: string) => {};
+
 const Request = (baseURL: string, timeout = 10000) => {
   const retryLimit = 3;
-  const retryQueue: Array<() => void> = [];
+  let retryQueue: Array<() => void> = [];
   let isRefresh = false;
   const instance = axios.create({
     baseURL,
@@ -39,7 +45,7 @@ const Request = (baseURL: string, timeout = 10000) => {
   instance.interceptors.response.use(
     (res: AxiosResponse<ResponseData>) => {
       const { data } = res;
-      const config = res as RequestConfig;
+      const config = res.config as RequestConfig;
       if (data.code !== 200 && !config.skipErrorHandler) {
         return Promise.reject(data);
       }
@@ -54,18 +60,65 @@ const Request = (baseURL: string, timeout = 10000) => {
       if (error.response?.status === 401) {
         return handleUnauthorized(orignalReq);
       }
-      return handleServerError(orignalReq);
+      return handleServerError(error, orignalReq);
     },
   );
 
-  const handleNetWorkError = () => {
-    // retry
+  const handleUnauthorized = (orignalReq: RequestConfig) => {
+    if (!isRefresh) {
+      isRefresh = true;
+      try {
+        const newToken = refreshToken();
+        localStorage.setItem("token", newToken);
+        retryQueue.forEach((cb) => cb());
+        return instance(orignalReq);
+      } catch (error) {
+        localStorage.removeItem("token");
+        window.location.href = "/login";
+        return Promise.reject(error);
+      } finally {
+        isRefresh = false;
+        retryQueue = [];
+      }
+    }
+    return new Promise((resolve) => {
+      retryQueue.push(() => {
+        resolve(instance(orignalReq));
+      });
+    });
   };
-  const handleUnauthorized = () => {
-    // token refresh
+
+  const handleNetWorkError = (orignalReq: RequestConfig) => {
+    if (orignalReq.retry ?? 0 < retryLimit) {
+      const delay = 1000 * Math.pow(2, orignalReq.retry || 0);
+      orignalReq.retry = (orignalReq.retry || 0) + 1;
+      return new Promise((resolve) =>
+        setTimeout(() => {
+          resolve(instance(orignalReq));
+        }, delay),
+      );
+    }
+    return Promise.reject(new Error("Network Error"));
   };
-  const handleServerError = () => {
-    // sort error to handle
+
+  const handleServerError = (error: AxiosError, orignalReq: RequestConfig) => {
+    const status = error.response?.status;
+    const data = error.response?.data as ResponseData;
+    switch (status) {
+      case 500:
+        showError("服务器内部错误");
+        break;
+      case 403:
+        showError("无权限");
+        break;
+      default:
+        showError(data?.message || "未知错误");
+        break;
+    }
+    return Promise.reject({
+      code: status || -1,
+      message: data.message || error.message,
+    });
   };
 
   return {
